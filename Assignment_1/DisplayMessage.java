012d91c89cc5362da90e4135f1a857722a7150a6
Siddhi-m-20 <114218813+Siddhi-m-20@users.noreply.github.com>
2023-08-30 18:23:31 +0530

    Add files via upload

---
+++ Assignment_1/DisplayMessage.java
@@ -0,0 +1,9 @@
+//Write a Java program to display a message.
+
+class DisplayMessage
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Hello, Welcome to Java programming!!");
+   }
+ }
