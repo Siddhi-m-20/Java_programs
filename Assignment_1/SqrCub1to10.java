e25e335a78e0c281ca24296be593fa2ba31d2bc1
Siddhi-m-20 <114218813+Siddhi-m-20@users.noreply.github.com>
2023-08-31 00:23:26 +0530

    Update SqrCub1to10.java

--- Assignment_1/SqrCub1to10.java
+++ Assignment_1/SqrCub1to10.java
@@ -14,3 +14,21 @@ public class SqrCub1to10 {
 
     }
 }
+
+
+
+
+
+===OUTPUT===
+Numbers from 1 to 10:
+Number  Square  Cube
+1       1       1
+2       4       8
+3       9       27
+4       16      64
+5       25      125
+6       36      216
+7       49      343
+8       64      512
+9       81      729
+10      100     1000
