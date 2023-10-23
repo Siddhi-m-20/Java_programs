8ef298ac1320bc712a14955d9469384a73c0fd90
Siddhi Manjarekar <114218813+Siddhi-m-20@users.noreply.github.com>
2023-10-23 22:53:17 +0530

    Add files via upload

---
+++ Assignment4/Interface/Operation.java
@@ -0,0 +1,8 @@
+package Interface;
+
+interface Operation {
+    double PI = 3.142; // Constant
+
+    double area();   // Method to calculate area
+    double volume(); // Method to calculate volume
+}
