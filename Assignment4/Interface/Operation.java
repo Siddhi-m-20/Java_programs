e91f4e971929a638bc24f3b733f81b831d4a2e05
Siddhi Manjarekar <114218813+Siddhi-m-20@users.noreply.github.com>
2023-10-27 18:10:55 +0530

    Delete Assignment4 directory

--- Assignment4/Interface/Operation.java
+++
@@ -1,8 +0,0 @@
-package Interface;
-
-interface Operation {
-    double PI = 3.142; // Constant
-
-    double area();   // Method to calculate area
-    double volume(); // Method to calculate volume
-}
